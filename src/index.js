9b92bfa90aa1bd99621e1fa934852296c6775f1a
Huzaifasid <siddiquihuzaifa68@gmail.com>
2023-10-10 19:54:47 +0500

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-import 'weather-icons/css/weather-icons.css';
+import "weather-icons/css/weather-icons.css";
 
-
-ReactDom.render(
-    <App />, document.getElementById("root"))
+ReactDom.render(<App />, document.getElementById("root"));
